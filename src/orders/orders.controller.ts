857f712c3da7b2cfb0262a2af7f38433eab92353
Long Dang <long.dang.clv@gmail.com>
2024-04-12 16:18:10 +0700

    Create exception filter and standardize response

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put, UseFilters, ForbiddenException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, UseFilters, HttpStatus } from '@nestjs/common';
 import { HttpExceptionFilter } from "../common/http-exception.filter"
 import { OrdersService } from './orders.service';
 import { Order } from './entities/order.entity';
@@ -13,29 +13,45 @@ export class OrdersController {
   async create(@Body() order: Partial<Order>): Promise<ResponseInterface> {
     await this.ordersService.create(order);
     return {
-      status: 201,
+      status: HttpStatus.CREATED,
       data: null,
       error: null
     }
   }
 
   @Get()
-  async findAll(): Promise<Order[]> {
-    return await this.ordersService.findAll();
+  async findAll(): Promise<ResponseInterface> {
+    return {
+      status: HttpStatus.OK,
+      data: await this.ordersService.findAll(),
+      error: null
+    }
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string): Promise<Order> {
-    return await this.ordersService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<ResponseInterface> {
+    return {
+      status: HttpStatus.OK,
+      data: await this.ordersService.findOne(+id),
+      error: null
+    }
   }
 
   @Put(':id')
-  async update(@Param('id') id: string, @Body() order: Order): Promise<Order> {
-    return await this.ordersService.update(+id, order);
+  async update(@Param('id') id: string, @Body() order: Order): Promise<ResponseInterface> {
+    return {
+      status: HttpStatus.OK,
+      data: await this.ordersService.update(+id, order),
+      error: null
+    }
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string): Promise<void> {
-    return await this.ordersService.remove(+id);
+  async remove(@Param('id') id: string): Promise<ResponseInterface> {
+    return {
+      status: HttpStatus.OK,
+      data: await this.ordersService.remove(+id),
+      error: null
+    }
   }
 }
